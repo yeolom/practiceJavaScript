2a067cd315498051ae743fdc964bb14bd7ee762c
lawrenceorolobo <lead@yeolom.com>
2023-03-31 07:21:26 +0100

    Practice 1
    I did practice some JS.

---
+++ app.js
@@ -0,0 +1,10 @@
+//To display in browser's console use below.
+console.log('First Output');
+
+//The approach to declare and assign a variable is.
+let userName = 'Lawrence';
+
+console.log('Hello '+userName);
+
+//To check the datatype of a value in a variable, do
+console.log(typeof userName);
